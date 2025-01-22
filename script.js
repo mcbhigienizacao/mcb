36926285ba0a18540f6313af59a904d8eb62d1fc
MCB <leomarmenasc@gmail.com>
2025-01-22 13:31:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let p_text = document.querySelector('.p-text');
+function selectBi() {
+    p_text.forEach((search)=>
+        search.classList.add('p-text')
+)
+    this.classList.remove('p-text');
+}
+let biExp = document.querySelector('.bi-plus-circle');
+biExp.addEventListener('click', function(){
+    p_text.classList.toggle('p-text')
+});
+
+let p_text_I = document.querySelector('.p-text-I');
+function selectBi_I() {
+    p_text_I.forEach((search)=>
+        search.classList.add('p-text-I')
+)
+    this.classList.remove('p-text-I');
+}
+let biExp_I = document.querySelector('.bi-I');
+biExp_I.addEventListener('click', function(){
+    p_text_I.classList.toggle('p-text-I')
+});
+
+let p_text_II = document.querySelector('.p-text-II');
+function selectBi_II() {
+    p_text_II.forEach((search)=>
+        search.classList.add('p-text-II')
+)
+    this.classList.remove('p-text-II');
+}
+let biExp_II = document.querySelector('.bi-II');
+biExp_II.addEventListener('click', function(){
+    p_text_II.classList.toggle('p-text-II')
+});
+
+let p_text_III = document.querySelector('.p-text-III');
+function selectBi_III() {
+    p_text_III.forEach((search)=>
+        search.classList.add('p-text-III')
+)
+    this.classList.remove('p-text-III');
+}
+let biExp_III = document.querySelector('.bi-III');
+biExp_III.addEventListener('click', function(){
+    p_text_III.classList.toggle('p-text-III')
+});
+
+let p_text_IV = document.querySelector('.p-text-IV');
+function selectBi_IV() {
+    p_text_IV.forEach((search)=>
+        search.classList.add('p-text-IV')
+)
+    this.classList.remove('p-text-IV');
+}
+let biExp_IV = document.querySelector('.bi-IV');
+biExp_IV.addEventListener('click', function(){
+    p_text_IV.classList.toggle('p-text-IV')
+});
